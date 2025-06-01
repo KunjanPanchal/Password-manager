17e4ebb5fa902883ef808fda21198657fe8c9c53
KunjanPanchal <kunjanpanchal06012004@gmail.com>
2025-06-01 12:14:11 +0530

    Hashrouter add

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,19 +3,19 @@ import Manager from "./components/Manager"
 import Login from "./components/Login"
 import Landing from "./components/Landing"
 import Register from "./components/Register"
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route,HashRouter } from 'react-router-dom';
 
 function App() {
   return (
     <>
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path="/" element={<Landing />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route path="/home" element={<Manager/>} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </>
   )
 }
